a9eb1e481778be9f29532158a80f35985cb61a4f
Rick Felix <Felirick@gmail.com>
2017-07-22 09:42:15 -0700

    Corrections

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
 
     // USE route returns home.html for any undefined GET routes.
     app.use(function (req, res) {
-        res.sendFile(path.join(__dirname + '/../public/home.html'));
+        res.sendFile(path.join(__dirname + '/../public/index.html'));
     });
 
 };
